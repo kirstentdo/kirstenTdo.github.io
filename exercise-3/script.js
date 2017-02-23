10b52c0dcfb8d947243b5809b78b298ca52b0241
kirstentdo <kirstentdo@gmail.com>
2017-02-23 15:07:01 -0500

    exercise-3 jquery

---
+++ exercise-3/script.js
@@ -0,0 +1,175 @@
+function shuffle(a) {
+    var j, x, i;
+    for (i = a.length; i; i--) {
+        j = Math.floor(Math.random() * i);
+        x = a[i - 1];
+        a[i - 1] = a[j];
+        a[j] = x;
+    }
+}
+
+$(document).ready(function() {
+
+//////////////////////////////////////////blackout paragraph///
+
+	var blackOutCss = {
+		'color': 'black',
+	    'background-color': 'black'
+	}
+
+	var revertCss = {
+		'color': 'black',
+	    'background-color': 'white'
+	}
+	$('#mw-content-text > p:nth-child(3)').hover(function() {
+			$(this).css(blackOutCss);
+			$(this).find('*').css(blackOutCss);	
+		}, 
+		function() {
+			$(this).css(revertCss);
+			$(this).find('*').css(revertCss);			
+	});
+
+	$('#mw-content-text > p:nth-child(56)').hover(function() {
+			$(this).css(blackOutCss);
+			$(this).find('*').css(blackOutCss);	
+		}, 
+		function() {
+			$(this).css(revertCss);
+			$(this).find('*').css(revertCss);			
+	});
+
+//////////////////////////////////////////shuffle///
+
+	var $elementToShuffle = $('#mw-content-text > p:nth-child(21)');
+	var originalText = $elementToShuffle.text();
+	$elementToShuffle.hover(
+		function() {
+			var text = originalText.trim().split(" ");
+			shuffle(text)
+			$(this).empty().append(text.join(" "));
+		},
+		function() {
+			$(this).empty().append(originalText);
+		}
+	);
+
+	var $elementToShuffle = $('#mw-content-text > p:nth-child(22)');
+	var originalText = $elementToShuffle.text();
+	$elementToShuffle.hover(
+		function() {
+			var text = originalText.trim().split(" ");
+			shuffle(text)
+			$(this).empty().append(text.join(" "));
+		},
+		function() {
+			$(this).empty().append(originalText);
+		}
+	);
+
+	var $elementToShuffle = $('#mw-content-text > p:nth-child(25)');
+	var originalText = $elementToShuffle.text();
+	$elementToShuffle.hover(
+		function() {
+			var text = originalText.trim().split(" ");
+			shuffle(text)
+			$(this).empty().append(text.join(" "));
+		},
+		function() {
+			$(this).empty().append(originalText);
+		}
+	);
+
+	var $elementToShuffle = $('#mw-content-text > p:nth-child(55)');
+	var originalText = $elementToShuffle.text();
+	$elementToShuffle.hover(
+		function() {
+			var text = originalText.trim().split(" ");
+			shuffle(text)
+			$(this).empty().append(text.join(" "));
+		},
+		function() {
+			$(this).empty().append(originalText);
+		}
+	);
+
+
+//////////////////////////////////////////blackout word anim///
+
+	var $elementToHighlight = $('#mw-content-text > p:nth-child(36)');
+	var originalText = $elementToHighlight.text();
+	$elementToHighlight.empty();
+	originalText.trim().split(" ").forEach(function(word) {
+		$elementToHighlight.append("<span class='blackout'>" + word + " </span>");
+	})
+
+	$('.blackout').mouseover(function() {
+		$(this).css('background-color', 'black');
+		$(this).animate({
+            backgroundColor: "white"
+    	}, 3000 );
+	});
+
+	var $elementToHighlight = $('#mw-content-text > p:nth-child(103)');
+	var originalText = $elementToHighlight.text();
+	$elementToHighlight.empty();
+	originalText.trim().split(" ").forEach(function(word) {
+		$elementToHighlight.append("<span class='blackout'>" + word + " </span>");
+	})
+
+	$('.blackout').mouseover(function() {
+		$(this).css('background-color', 'black');
+		$(this).animate({
+            backgroundColor: "white"
+    	}, 3000 );
+	});
+
+	var $elementToHighlight = $('#mw-content-text > p:nth-child(46)');
+	var originalText = $elementToHighlight.text();
+	$elementToHighlight.empty();
+	originalText.trim().split(" ").forEach(function(word) {
+		$elementToHighlight.append("<span class='blackout'>" + word + " </span>");
+	})
+
+	$('.blackout').mouseover(function() {
+		$(this).css('background-color', 'black');
+		$(this).animate({
+            backgroundColor: "white"
+    	}, 2000 );
+	});
+
+//////////////////////////////////////////blackout word perm///
+
+	var $elementToHighlight = $('#mw-content-text > p:nth-child(59)');
+	var originalText = $elementToHighlight.text();
+	$elementToHighlight.empty();
+	originalText.trim().split(" ").forEach(function(word) {
+		$elementToHighlight.append("<span class='blackout'>" + word + " </span>");
+	})
+
+	$('.blackout').mouseover(function() {
+		$(this).css('background-color', 'black');
+	});
+
+	var $elementToHighlight = $('#mw-content-text > p:nth-child(23)');
+	var originalText = $elementToHighlight.text();
+	$elementToHighlight.empty();
+	originalText.trim().split(" ").forEach(function(word) {
+		$elementToHighlight.append("<span class='blackout'>" + word + " </span>");
+	})
+
+	$('.blackout').mouseover(function() {
+		$(this).css('background-color', 'black');
+	});
+
+	var $elementToHighlight = $('#mw-content-text > p:nth-child(47)');
+	var originalText = $elementToHighlight.text();
+	$elementToHighlight.empty();
+	originalText.trim().split(" ").forEach(function(word) {
+		$elementToHighlight.append("<span class='blackout'>" + word + " </span>");
+	})
+
+	$('.blackout').mouseover(function() {
+		$(this).css('background-color', 'black');
+	});
+});
